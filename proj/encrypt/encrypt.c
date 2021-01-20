6921ff8f8c453d0de654f0e128bd3338d57cb598
Han Li <lih1119@my.sduhsd.net>
2021-01-20 22:48:23 +0000

    updating project files

--- proj/encrypt/encrypt.c
+++ proj/encrypt/encrypt.c
@@ -22,6 +22,21 @@ char shiftChar(char c, int shift, int direction)
   //   shiftChar('c', 3, 1) : 'f'
   //   shiftChar('S', 2, 0) : 'P'
   //   shiftChar('b', 3, 0) : '8'
+  char* x = strchr(CHARS, c);
+  int index = (int)(x-CHARS);
+  if (direction == 0) {
+    index = index - shift;
+    if (index < 0) {
+      index += strlen(CHARS);
+    }
+  }
+  else {
+    index = index + shift;
+    if (index > strlen(CHARS)-1) {
+      index -= strlen(CHARS);
+    }
+  }
+  return CHARS[index];
 }
 
 
